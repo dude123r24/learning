f2d60186e980f381f7780de78eff16bdc3cdc71b
Amit Sanghvi <amit2u@hotmail.com>
2020-06-26 10:36:42 +0100

    update

--- python/input_bool.py
+++ python/input_bool.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-first=input ("What is your first name?")
-last=input ("What is your last name?")
+
+first=input ("What is your first name? ")
+
+last=input ("What is your last name? ")
+
 if first or last:
 	print ("OR: The user has a first or last name")
 else:
 	print ("OR: User does not have first or last name")
+    
 if first or last:
 	print ("OR: The user has a first or last name")
+
 if first and last:
 	print ("AND: The user has a first and last name")
 else:
